b828ceeeb5ba8b51ffda36d00fe8e19578273941
Eremel <richardbailey@outlook.com>
2024-09-11 20:42:57 +0100

    art gallery collection bug fix

--- objects/jokers/art_gallery.lua
+++ objects/jokers/art_gallery.lua
@@ -12,7 +12,7 @@ SMODS.Joker({
 	config = {extra = {chips_add = 20}},
     loc_vars = function(self, info_queue, card)
         if card and Ortalab.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'flare'} end
-		local total_cards = #G.jokers.cards + #G.consumeables.cards
+		local total_cards = (G.jokers and #G.jokers.cards or 0) + (G.consumeables and #G.consumeables.cards or 0)
 		return {vars = {card.ability.extra.chips_add, total_cards*card.ability.extra.chips_add}}
     end,
 	calculate = function(self, card, context)
