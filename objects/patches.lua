bab038b1dc5ddbc34c7c9c64b5d5ae16db04b8c3
Eremel <richardbailey@outlook.com>
2024-10-12 23:44:21 +0100

    loteria patch

--- objects/patches.lua
+++ objects/patches.lua
@@ -350,6 +350,35 @@ SMODS.Tag({
     end
 })
 
+SMODS.Tag({
+    key = 'loteria',
+    atlas = 'patches',
+    pos = {x = 2, y = 4},
+    discovered = false,
+    config = {type = 'shop_final_pass', packs = 2},
+    loc_vars = function(self, info_queue, card)
+        if Ortalab.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'eremel'} end
+        info_queue[#info_queue + 1] = G.P_CENTERS['p_ortalab_mid_loteria_1']
+        return {vars = {self.config.packs}}
+    end,
+    apply = function(tag, context)
+        tag:yep('+', G.C.GREEN,function()
+            for i=1, tag.config.packs do
+                local pack = Card(G.shop_booster.T.x + G.shop_booster.T.w/2,
+                G.shop_booster.T.y, G.CARD_W*1.27, G.CARD_H*1.27, G.P_CARDS.empty, G.P_CENTERS['p_ortalab_mid_loteria_'..i], {bypass_discovery_center = true, bypass_discovery_ui = true})
+                create_shop_card_ui(pack, 'Booster', G.shop_booster)
+                pack.ability.booster_pos = #G.shop_booster.cards + 1
+                pack.ability.couponed = true
+                pack:start_materialize()
+                G.shop_booster:emplace(pack)
+                pack:set_cost()
+            end
+            return true
+        end)
+        
+    end
+})
+
 local skip_blind = G.FUNCS.skip_blind
 G.FUNCS.skip_blind = function(e)
     local _tag = e.UIBox:get_UIE_by_ID('tag_container').config.ref_table
