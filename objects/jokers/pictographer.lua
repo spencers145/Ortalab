f8a2b7ab466f52395c0af5ae7a9bce1768299f33
Eremel <richardbailey@outlook.com>
2024-10-15 23:47:06 +0100

    pictographer artist credit

--- objects/jokers/pictographer.lua
+++ objects/jokers/pictographer.lua
@@ -2,8 +2,8 @@ SMODS.Joker({
 	key = "pictographer",
 	atlas = "jokers",
 	pos = {x=3,y=10},
-	rarity = 1,
-	cost = 4,
+	rarity = 2,
+	cost = 5,
 	unlocked = true,
 	discovered = false,
 	blueprint_compat = true,
@@ -11,7 +11,7 @@ SMODS.Joker({
 	perishable_compat = false,
 	config = {extra = {money = 30, count = 4, current = 0}},
 	loc_vars = function(self, info_queue, card)
-        if card and Ortalab.config.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'eremel'} end
+        if card and Ortalab.config.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'gappie'} end
 		return {vars = {card.ability.extra.money, card.ability.extra.count, card.ability.extra.current}}
 	end,
 	calculate = function(self, card, context)
