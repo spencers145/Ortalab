a01c60be9f8f18908a47f0929dbf80de92434fab
Eremel <richardbailey@outlook.com>
2024-10-29 17:10:06 +0000

    few fixes

--- objects/jokers/mill.lua
+++ objects/jokers/mill.lua
@@ -29,7 +29,7 @@ function Card:change_suit(new_suit)
     local change = self.base.suit ~= new_suit
     card_change_suit(self, new_suit)
     if not change then return end
-    local scaling_joker = SMODS.find_card('j_ortalab_loteria_5')
+    local scaling_joker = SMODS.find_card('j_ortalab_mill')
     for _, card in pairs(scaling_joker) do        
         card.ability.extra.xmult = card.ability.extra.xmult + card.ability.extra.gain
         card_eval_status_text(card, 'extra', nil, nil, nil, {message = localize{type = 'variable', key = 'a_xmult', vars = {card.ability.extra.xmult}}})
@@ -41,7 +41,7 @@ function SMODS.change_base(card, suit, rank)
     local change = suit and card.base.suit ~= suit
     local card = smods_change_base(card, suit, rank)
     if change and not Ortalab.harp_usage then
-        local scaling_joker = SMODS.find_card('j_ortalab_loteria_5')
+        local scaling_joker = SMODS.find_card('j_ortalab_mill')
         for _, card in pairs(scaling_joker) do        
             card.ability.extra.xmult = card.ability.extra.xmult + card.ability.extra.gain
             card_eval_status_text(card, 'extra', nil, nil, nil, {message = localize{type = 'variable', key = 'a_xmult', vars = {card.ability.extra.xmult}}})
