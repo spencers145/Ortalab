e05abe537118dcc1de80e11f4a26d870606644a7
Eremel <richardbailey@outlook.com>
2024-12-13 23:29:17 +0000

    manifest file

--- main.lua
+++ main.lua
@@ -1,14 +1,3 @@
---- STEAMODDED HEADER
---- MOD_NAME: Ortalab
---- MOD_ID: ortalab
---- PREFIX: ortalab
---- MOD_AUTHOR: [Balatro Discord]
---- MOD_DESCRIPTION: Every action has an opposite reaction. In another world, in the nation of "Virtue", a simple indie developer created Ortalab, which so happened to be the opposite of our world's Balatro. This mod is intended to port everything from that parallel world to Balatro. Within this demo, it includes 75 Jokers, 10 Decks, 7 sets of Coupons, a new Consumable Type with Boosters - Loteria Cards, 8 Enhancements, 4 Editions, 12 patches, and 2 Stakes for you to try out.
---- BADGE_COLOUR: 990000
---- BADGE_TEXT_COLOUR: E28585
---- VERSION: 0.9-demo-2.0.1
---- PRIORITY: -5
-
 Ortalab = SMODS.current_mod
 Ortalab.load_table = {
     jokers = true,
