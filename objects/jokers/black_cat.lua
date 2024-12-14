705517f3f1587ce10b70d2356d05003f6688b22a
Eremel <richardbailey@outlook.com>
2024-12-14 12:31:10 +0000

    black cat adjustment

--- objects/jokers/black_cat.lua
+++ objects/jokers/black_cat.lua
@@ -9,9 +9,9 @@ SMODS.Joker({
 	blueprint_compat = true,
 	eternal_compat = true,
 	perishable_compat = false,
-	config = {extra = {chance = 4}},
+	config = {extra = {num = 3, chance = 4}},
 	loc_vars = function(self, info_queue, card)
         if card and Ortalab.config.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'kosze'} end
-		return {vars = {3*G.GAME.probabilities.normal, card.ability.extra.chance}}
+		return {vars = {card.ability.extra.num*G.GAME.probabilities.normal, card.ability.extra.chance}}
 	end
 })
