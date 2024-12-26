d53fe9b20ffd72826fa3cc6c59efef6034725355
Eremel <richardbailey@outlook.com>
2024-12-26 13:17:38 +0000

    Switch to Ortalab.Curse

--- objects/curses.lua
+++ objects/curses.lua
@@ -198,6 +198,8 @@ function Card:calculate_curse(context)
     	if o then return o end
     end
 end
+
+Ortalab.Curse({
     key = 'corroded',
     atlas = 'curses',
     pos = {x = 0, y = 0},
@@ -208,39 +210,39 @@ end
     end,
     loc_vars = function(self, info_queue, card)
         if card and Ortalab.config.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'gappie'} end
-		return {vars = {card.ability.seal.extra.base, card.ability.seal.extra.gain}}
+		return {vars = {card.ability.curse.extra.base, card.ability.curse.extra.gain}}
     end,
     calculate = function(self, card, context)
         if context.discard then
             card_eval_status_text(card, 'extra', nil, nil, nil, {message = localize('ortalab_corroded'), colour = G.C.RED})
-            card.ability.seal.extra.base = card.ability.seal.extra.base + card.ability.seal.extra.gain
+            card.ability.curse.extra.base = card.ability.curse.extra.base + card.ability.curse.extra.gain
             card.corroded_discard = true
         end
         if context.cardarea and context.cardarea == G.play and not context.repetition and not context.individual then
             return {
-                message = '-'..localize('$')..card.ability.seal.extra.base,
-                p_dollars = -card.ability.seal.extra.base,
+                message = '-'..localize('$')..card.ability.curse.extra.base,
+                p_dollars = -card.ability.curse.extra.base,
                 colour = G.C.RED,
             }
         end
     end
 })
 
-SMODS.Seal({
+Ortalab.Curse({
     key = 'possessed',
     atlas = 'curses',
     pos = {x = 1, y = 0},
     badge_colour = HEX('82b4f4')
 })
 
-SMODS.Seal({
+Ortalab.Curse({
     key = 'restrained',
     atlas = 'curses',
     pos = {x = 2, y = 0},
     badge_colour = HEX('d78532')
 })
 
-SMODS.Seal({
+Ortalab.Curse({
     key = 'infected',
     atlas = 'curses',
     pos = {x = 3, y = 0},
