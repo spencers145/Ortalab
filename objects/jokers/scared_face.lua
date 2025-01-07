ba733194287e54905f2818915cd6c66521350d11
Eremel <richardbailey@outlook.com>
2025-01-07 16:26:04 +0000

    Balance change

--- objects/jokers/scared_face.lua
+++ objects/jokers/scared_face.lua
@@ -9,7 +9,7 @@ SMODS.Joker({
 	blueprint_compat = true,
 	eternal_compat = true,
 	perishable_compat = true,
-    config = {extra = {chips = 30}},
+    config = {extra = {chips = 20}},
 	loc_vars = function(self, info_queue, card)
         if card and Ortalab.config.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'gappie'} end
         return {vars = {card.ability.extra.chips}}
