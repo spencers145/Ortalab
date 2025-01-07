cd558643c49b23caea5ba273d0b8b85eedaefe2f
Eremel <richardbailey@outlook.com>
2025-01-07 16:25:50 +0000

    Balance change

--- objects/jokers/frowny_face.lua
+++ objects/jokers/frowny_face.lua
@@ -9,7 +9,7 @@ SMODS.Joker({
 	blueprint_compat = true,
 	eternal_compat = true,
 	perishable_compat = true,
-	config = {extra = {mult = 4}},
+	config = {extra = {mult = 3}},
     loc_vars = function(self, info_queue, card)
         if card and Ortalab.config.artist_credits then info_queue[#info_queue+1] = {generate_ui = ortalab_artist_tooltip, key = 'golddisco'} end
         return {vars = {card.ability.extra.mult}}
