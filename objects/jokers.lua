ebf98f04dad9185028ce336391ecb67dfa72f349
Eremel <richardbailey@outlook.com>
2025-01-24 02:30:50 +0000

    adjust Ortalab.remove_joker

--- objects/jokers.lua
+++ objects/jokers.lua
@@ -184,19 +184,16 @@ end
 
 function Ortalab.remove_joker(card)
     G.E_MANAGER:add_event(Event({
+        trigger = 'after',
+        delay = 0.7,
         func = function()
             play_sound('tarot1')
             card.T.r = -0.2
             card:juice_up(0.3, 0.4)
             card.states.drag.is = true
             card.children.center.pinch.x = true
-            G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
-                func = function()
-                        G.jokers:remove_card(card)
-                        card:remove()
-                        card = nil
-                    return true
-            end}))
+            card:start_dissolve()
+            card = nil
             return true
     end}))
 end
